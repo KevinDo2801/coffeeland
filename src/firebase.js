163d4a3bc46559d1932b094ef3a3cd6492b7229b
Kevindo28012003 <kietdo@usf.edu>
2025-06-23 00:04:05 -0400

    add header

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDe0W9YJSyMVSxLUJeXRwGtfeRr0NrWhXI",
-  authDomain: "cafeland-29955.firebaseapp.com",
-  projectId: "cafeland-29955",
-  storageBucket: "cafeland-29955.firebasestorage.app",
-  messagingSenderId: "1076741158383",
-  appId: "1:1076741158383:web:c46feb12412f47921df21a",
-  measurementId: "G-9RECWG3DQ3"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
